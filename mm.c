bbaf452876021220ee134b3869f9f36b806c85b6
harrisonBroCode <harrison.letourneau@gmail.com>
2025-11-21 20:07:48 -0700

    Finished naive sbrk imp, including implicit free list for mmfree with coalescing -> conv to mmap from sbrk

--- mm.c
+++ mm.c
@@ -5,8 +5,8 @@
  * new page as needed.  A block is pure payload. There are no headers or
  * footers.  Blocks are never coalesced or reused.
  *
- * NOTE TO STUDENTS: Replace this header comment with your own header
- * comment that gives a high level description of your solution.
+ * Building allocator, starting with naive approach.
+ * Iteration 1: chunk based allocator.
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -26,25 +26,74 @@
 /* rounds up to the nearest multiple of mem_pagesize() */
 #define PAGE_ALIGN(size) (((size) + (mem_pagesize()-1)) & ~(mem_pagesize()-1))
 
-#define CHUNK_SIZE (1<<14)
-/* rounds up to the nearest multiple of mem_pagesize() */
-#define CHUNK_ALIGN(size) (((size) + (mem_pagesize()-1)) & ~(mem_pagesize()-1))
+
+/* Additional constants and macros*/
+
+#define CHUNK_SIZE (1<<14) // Chunks are 4 pages
+
+/* rounds up to the nearest multiple of CHUNK_SIZE */
+#define CHUNK_ALIGN(size) (((size) + (CHUNK_SIZE-1)) & ~(CHUNK_SIZE-1))
+
+
+// For block header
+
+#define OVERHEAD sizeof(block_header) + sizeof(block_footer)
+
+/* Get to header from payload pointer bp */
+#define HDRP(bp) ((char *) (bp) - sizeof(block_header))
+
+/* Get to footer from payload pointer bp */
+#define FTRP(bp) ((char *) (bp) + GET_SIZE(HDRP(bp)) - OVERHEAD)
+
+#define GET_SIZE(p) ((block_header *) (p))->size
+
+#define GET_ALLOC(p) ((block_header *) (p))->allocated
+
+#define NEXT_BLKP(bp) ((char *) (bp) + GET_SIZE(HDRP(bp)))
+
+#define PREV_BLKP(bp) ((char *) (bp) - GET_SIZE((char *)  (bp) - OVERHEAD))
+
+
+
+
 
 void *current_avail = NULL;
-int current_avail_size = 0;
+size_t current_avail_size = 0;
+
+/* Useful structs*/
+
+/* Block header */
+typedef struct {
+  size_t size;
+  char allocated;
+} block_header;
+
+/* Block footer with filler for allignment to 16 b */
+typedef struct {
+  size_t size;
+  int filler;
+} block_footer;
 
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
+  // Butterfly knife guy mminit
   // Freelist head null
   // something else null
   // extend by1 
   // return return -1 if failed 0 if success
-  current_avail = NULL;
-  current_avail_size = 0;
-  
+
+
+  sbrk(sizeof(block_header));
+  first_bp = sbrk(0);
+
+  GET_SIZE(HDRP(first_bp)) = 0;
+  GET_ALLOC(HDRP(first_bp)) = 1;
+
+  mm_malloc(0); // Never gets freed to avoid prev pointer error
+
   return 0;
 }
 
@@ -72,18 +121,21 @@ void *mm_malloc(size_t size)
   
   // return p;
 
-  size_t newsize = ALIGN(size);
-  void *p;
+  int new_size = ALIGN(size + OVERHEAD);
+  void *bp = first_bp;
 
-  if (current_avail_size < newsize) {
-    current_avail = CHUNK_ALIGN(newsize);
-    current_avail_size = CHUNK_ALIGN(newsize) ;
+  while (GET_SIZE(HDRP(bp)) != 0) {
+    if (!GET_ALLOC(HDRP(bp))
+      && (GET_SIZE(HDRP(bp)) >= new_size)) {
+      set_allocated(bp, new_size);
+      return bp;
+    }
+    bp = NEXT_BLKP(bp);
   }
 
-  p = current_avail;
-  current_avail += newsize; 
-  current_avail_size -= newsize;
-  return p;
+  extend(new_size);
+  set_allocated(bp, new_size);
+  return bp;
 }
 
 /*
@@ -91,4 +143,82 @@ void *mm_malloc(size_t size)
  */
 void mm_free(void *ptr)
 {
+  GET_ALLOC(HDRP(bp)) = 0;
+  // coalesce(bp);
+}
+
+/* Helper Functions */
+
+void extend(size_t new_size) {
+  size_t chunk_size = CHUNK_ALIGN(new_size);
+  void *bp = sbrk(chunk_size);
+
+  GET_SIZE(HDRP(bp)) = chunk_size;
+  GET_SIZE(FTRP(bp)) = chunk_size;
+  GET_ALLOC(HDRP(bp)) = 0;
+
+  GET_SIZE(HDRP(NEXT_BLKP(bp))) = 0;
+  GET_ALLOC(HDRP(NEXT_BLKP(bp))) = 1;
+
+}
+
+void set_allocated(void *bp, size_t size) {
+  size_t extra_size = GET_SIZE(HDRP(bp)) - size;
+
+  if (extra_size > ALIGN(1 + OVERHEAD)) {
+    GET_SIZE(HDRP(bp)) = size;
+    GET_SIZE(FTRP(bp)) = size;  
+    GET_SIZE(HDRP(NEXT_BLKP(bp))) = extra_size;
+    GET_SIZE(FTRP(NEXT_BLKP(bp))) = extra_size;
+    GET_ALLOC(HDRP(NEXT_BLKP(bp))) = 0;
+  }
+
+  GET_ALLOC(HDRP(bp)) = 1;
+}
+
+
+void *coalesce (void *bp) {
+  size_t prev_alloc = GET_ALLOC(HDRP(PREV_BLKP(bp)));
+  size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+  size_t size = GET_SIZE(HDRP(bp));
+
+
+  // Case 1: Nothing to do
+
+  if (prev_alloc && next_alloc) {
+    /* Do nothing */
+  }
+
+  // Case 2: Freed block before another free block 
+
+  else if (prev_alloc && !next_alloc) {
+    size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+    GET_SIZE(HDRP(bp)) = size;
+    GET_SIZE(FTRP(bp)) = size;
+  }
+
+  // Case 3: Freed block after another free block
+
+  else if (!prev_alloc && next_alloc) {
+    size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+    GET_SIZE(FTRP(bp)) = size;
+    GET_SIZE(HDRP(PREV_BLKP(bp))) = size;
+    bp = PREV_BLKP(bp);
+  }
+
+  // Case 4: Freed block between free blocks
+
+  else {
+    size += (GET_SIZE(HDRP(PREV_BLKP(bp)))
+              + GET_SIZE(HDRP(NEXT_BLKP(bp))));
+    
+    GET_SIZE(HDRP(PREV_BLKP(bp))) = size;
+    GET_SIZE(FTRP(NEXT_BLKP(bp))) = size;
+    bp = PREV_BLKP(bp);
+  }
+
+
+  // Pointer to payload of coalesced free block
+  return bp;
+  
 }
